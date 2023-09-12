a0f39aeed63368babd94403e0ab57b9c4f2107a5
Shaun Sylvain David <shaunsylvaindavid@gmail.com>
2023-09-12 23:08:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,126 @@
+function submitForm(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  // Check if the email exists in the database
+  CrossDB.orderByChild("email")
+    .equalTo(email)
+    .once("value")
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        alert("You have already attempted the quiz.");
+        return;
+      } else {
+        // New user, start with a score of 0
+        score = 0;
+        window.location.href = `crossword.html?name=${name}&email=${email}`;
+      }
+    });
+}
+
+let score = 0;
+let usersData = [];
+
+function checkCompletion() {
+  const cells = document.querySelectorAll("td");
+  let allAnswersCorrect = true;
+  for (const cell of cells) {
+    if (cell.classList.length > 0 && cell.style.color !== "black") {
+      allAnswersCorrect = false;
+      break;
+    }
+  }
+
+  if (allAnswersCorrect) {
+    puzzleCompleted = true;
+    alert("Congratulations! You've completed the entire puzzle.");
+    disableForm();
+  }
+}
+
+function checkAnswer(event) {
+  event.preventDefault();
+  console.log("Function executed");
+  const clueNumber = document.getElementById("clueNumber").value;
+  const answer = document.getElementById("answer").value.trim().toLowerCase();
+
+  const correctAnswer = answers[clueNumber];
+
+  if (correctAnswer && answer === correctAnswer) {
+    const cells = document.querySelectorAll("td");
+    for (const cell of cells) {
+      if (cell.classList.contains(`'${correctAnswer}'`)) {
+        cell.style.color = "black";
+      }
+    }
+    score += 5;
+    checkCompletion();
+  } else {
+    alert("Wrong answer. Please try again.");
+  }
+
+  document.getElementById("clueNumber").value = "";
+  document.getElementById("answer").value = "";
+
+  return false;
+}
+
+function finishPuzzle() {
+  clearInterval(countdown);
+  const timeTaken = 420 - timesecond;
+  const crosswordContainer = document.querySelector(".Tab");
+  const form = document.querySelector("form");
+  const content = document.getElementById("content");
+  crosswordContainer.style.display = "none";
+  form.style.display = "none";
+  content.style.display = "none";
+
+  const resultsMessage = document.createElement("div");
+  resultsMessage.innerHTML = `Time Taken: ${Math.floor(timeTaken / 60)} mins ${
+    timeTaken % 60
+  } secs<br/>Your Score: ${score}`;
+  resultsMessage.classList.add("results-text");
+  resultsMessage.style.fontSize = "2.0em";
+  resultsMessage.style.textAlign = "center";
+  document.body.appendChild(resultsMessage);
+  const finishBtn = document.getElementById("finishBtn");
+  finishBtn.style.display = "none";
+
+  const params = new URLSearchParams(window.location.search);
+  const name = params.get("name");
+  const email = params.get("email");
+
+  // Call updateScore function here
+  updateScore(name, email, score);
+}
+const firebaseConfig = {
+  apiKey: "AIzaSyDFjHUU6aEXYx8Xu4oI0gRmoZ3IJfBaJhw",
+  authDomain: "techczar-cross.firebaseapp.com",
+  databaseURL: "https://techczar-cross-default-rtdb.firebaseio.com",
+  projectId: "techczar-cross",
+  storageBucket: "techczar-cross.appspot.com",
+  messagingSenderId: "1021179059862",
+  appId: "1:1021179059862:web:b6450d60ec83151327acb7",
+};
+firebase.initializeApp(firebaseConfig);
+const CrossDB = firebase.database().ref("signupForm");
+function updateScore(name, email, score) {
+  CrossDB.push().set({
+    name: name,
+    email: email,
+    score: score,
+  });
+}
+const answers = {
+  1: "firewall",
+  2: "wizard",
+  3: "forbidden",
+  4: "scrollsql",
+  5: "cookies",
+  6: "shield",
+  7: "python",
+  8: "soulstack",
+  9: "spellcode",
+};
---
+++ timer.js
@@ -0,0 +1,95 @@
+const timeh = document.querySelector("h2");
+let timesecond = localStorage.getItem("startTime") || 420;
+
+displaytime(timesecond);
+
+const countdown = setInterval(() => {
+  timesecond--;
+  displaytime(timesecond);
+  if (timesecond <= 0 || timesecond < 1) {
+    clearInterval(countdown);
+    endtime(score);
+  }
+}, 1000);
+
+function displaytime(sec) {
+  const min = Math.floor(sec / 60);
+  const second = Math.floor(sec % 60);
+  timeh.innerHTML = `${min < 10 ? "0" : ""}${min}:${
+    second < 10 ? "0" : ""
+  }${second}`;
+}
+
+function endtime(score) {
+  const crosswordContainer = document.querySelector(".Tab");
+  const form = document.querySelector("form");
+  const content = document.getElementById("content");
+  const header = document.querySelector("header");
+  header.style.display = "none";
+  crosswordContainer.style.display = "none";
+  form.style.display = "none";
+  content.style.display = "none";
+  const finishBtn = document.getElementById("finishBtn");
+  finishBtn.style.display = "none";
+
+  const timeoutContainer = document.createElement("div");
+  timeoutContainer.style.display = "flex";
+  timeoutContainer.style.flexDirection = "column";
+  timeoutContainer.style.alignItems = "center";
+  timeoutContainer.style.justifyContent = "center";
+  timeoutContainer.style.position = "fixed";
+  timeoutContainer.style.top = "10%";
+  timeoutContainer.style.right = "0";
+  timeoutContainer.style.bottom = "-50px";
+  timeoutContainer.style.left = "0";
+  timeoutContainer.style.zIndex = "9999";
+
+  const timeoutMessage = document.createElement("div");
+  timeoutMessage.textContent = `TIME OUT - Your Score: ${score}`;
+  timeoutMessage.classList.add("results-text");
+  timeoutMessage.style.fontSize = "2.0em";
+  timeoutMessage.style.textAlign = "center";
+  timeoutContainer.appendChild(timeoutMessage);
+
+  const userName = prompt("Enter your name:");
+  if (userName) {
+    usersData.push({ name: userName, score: score });
+
+    // Sort and display rankings
+    sortUsersByScore();
+    displayRankingsAndScores();
+
+    // Save user data
+    saveUserData();
+  }
+
+  const showScoreboardButton = document.createElement("button");
+  showScoreboardButton.textContent = "Show Scoreboard";
+  showScoreboardButton.id = "showScoreboard1";
+
+  showScoreboardButton.addEventListener("click", function () {
+    window.location.href = "score.html";
+  });
+
+  timeoutContainer.appendChild(showScoreboardButton);
+  document.body.appendChild(timeoutContainer);
+}
+
+function clearScoreboard() {
+  const password = prompt("Enter the password to clear the scoreboard:");
+
+  // Check if the password is correct (replace 'your_password_here' with your actual password)
+  if (password === "Techczar@2023") {
+    usersData = [];
+    saveUserData();
+    displayRankingsAndScores();
+    alert("Scoreboard cleared successfully.");
+  } else {
+    alert("Incorrect password. Scoreboard not cleared.");
+  }
+}
+function saveStartTime() {
+  localStorage.setItem("startTime", timesecond);
+}
+
+window.addEventListener("beforeunload", saveStartTime);
