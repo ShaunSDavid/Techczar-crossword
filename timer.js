4d19ca8a9f10cd536c21f56b6e8685877cd9d138
Shaun Sylvain David <shaunsylvaindavid@gmail.com>
2023-09-13 11:23:36 +0530

    time updates

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 const timeh = document.querySelector("h2");
-let timesecond = localStorage.getItem("startTime") || 420;
+let timesecond = localStorage.getItem("startTime") || 10;
 
 displaytime(timesecond);
 
